551837768abb21600f2362e83dec03bce9011dfa
nqkhaii <Khaikun2003vd@gmail.com>
2024-09-28 23:57:24 +0700

    init project

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package hoidanit;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld with java and hỏi dân IT");
+		System.out.println("HelloWorld with java and hỏi dân IT");
+		System.out.println("HelloWorld with java and hỏi dân IT");
+		System.out.println("HelloWorld with java and hỏi dân IT");
+	}
+
+}
